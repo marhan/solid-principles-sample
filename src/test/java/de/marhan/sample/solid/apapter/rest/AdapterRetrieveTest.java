c2cad788e9422ae431f42849dded94d3936024cf
Markus Hanses <markus.hanses@immonet.de>
2019-04-12 16:03:28 +0200

    Parameterized tests. Not working with gradle, yet.

--- src/test/java/de/marhan/sample/solid/apapter/rest/AdapterRetrieveTest.java
+++ src/test/java/de/marhan/sample/solid/apapter/rest/AdapterRetrieveTest.java
@@ -3,8 +3,9 @@ package de.marhan.sample.solid.apapter.rest;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -29,19 +30,23 @@ class AdapterRetrieveTest {
 				.baseUri(determineBaseUri());
 	}
 
-	@Test
-	void retrieve() {
+	@ParameterizedTest
+	@ValueSource(strings = {"well", "poorly"})
+	void retrieve(String category) {
 		requestSpecification
 				.baseUri(determineBaseUri())
+				.pathParam("category", category)
 				.when()
-				.get("/well/apartments")
+				.get("/{category}/apartments")
 				.then().log().all()
 				.statusCode(200)
 				.body("$", hasSize(2))
 				.body("findAll { it.apartmentId == 'a81b0db2-7114-4f35-8776-751acb3730ca'}.city", hasItems("Hamburg"))
 				.body("findAll { it.apartmentId == 'a81b0db2-7114-4f35-8776-751acb3730ca'}.street", hasItems("Helmholtzstraße 2"))
+				.body("findAll { it.apartmentId == 'a81b0db2-7114-4f35-8776-751acb3730ca'}.status", hasItems("free"))
 				.body("findAll { it.apartmentId == '6226560e-2e9a-4dad-8854-f996ed47e250'}.city", hasItems("Berlin"))
-				.body("findAll { it.apartmentId == '6226560e-2e9a-4dad-8854-f996ed47e250'}.street", hasItems("Westarpstraße 3"));
+				.body("findAll { it.apartmentId == '6226560e-2e9a-4dad-8854-f996ed47e250'}.street", hasItems("Westarpstraße 3"))
+				.body("findAll { it.apartmentId == '6226560e-2e9a-4dad-8854-f996ed47e250'}.status", hasItems("free"));
 	}
 
 	private String determineBaseUri() {
